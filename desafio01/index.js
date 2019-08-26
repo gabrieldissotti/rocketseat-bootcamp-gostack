b4dc448a2fdd79dd0913b091f2453c76330b5458
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-08-26 14:01:06 -0300

    feat(desafio01): routes
    added some end-points

--- desafio01/index.js
+++ desafio01/index.js
@@ -2,4 +2,35 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
+let projects = []
+
+server.get('/projects', (req, res) => {
+  return res.json(projects)
+})
+
+server.post('/projects', (req, res) => {
+  projects.push(req.body)
+
+  return res.json(projects)
+})
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params
+  const { title } = req.body
+
+  projects.find( ({id}) => id == id ).title = title
+
+  return res.json(projects)
+})
+
+server.delete('/projects/:id', (req, res) => {
+  projects = projects.filter(({id}) => Number(id) !== Number(req.params.id))
+
+  return res.json(projects)
+})
+
+
+
 server.listen(3000)
