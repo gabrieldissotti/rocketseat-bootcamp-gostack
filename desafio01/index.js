96ae83eabda1f2bece8d9471e1d5795b87fe8a8f
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-08-27 12:51:04 -0300

    feat(desafio01): exercises
    finished

--- desafio01/index.js
+++ desafio01/index.js
@@ -5,27 +5,62 @@ const server = express()
 server.use(express.json())
 
 let projects = []
+let counter = 0
 
-server.get('/projects', (req, res) => {
+
+function checkIfProjectExists(req, res, next){
+  const { id } = req.params
+  if(!id || !projects.find(({id: projectId}) => projectId == id)){
+    return res.status(400).json({ error: 'project not found'})
+  }
+
+  return next()
+}
+
+function countRequest(req, res, next){
+  counter++
+
+  console.log(counter)
+  return next()
+}
+
+server.get('/projects', countRequest, (req, res) => {
   return res.json(projects)
 })
 
-server.post('/projects', (req, res) => {
+server.get('/projects/:id', countRequest, checkIfProjectExists, (req, res) => {
+  const { id } = req.params
+
+  return res.json(projects.find(({id: projectId}) => projectId == id))
+})
+
+
+server.post('/projects', countRequest, (req, res) => {
   projects.push(req.body)
 
   return res.json(projects)
 })
 
-server.put('/projects/:id', (req, res) => {
+server.put('/projects/:id', countRequest, checkIfProjectExists, (req, res) => {
   const { id } = req.params
   const { title } = req.body
 
-  projects.find( ({id}) => id == id ).title = title
+  projects.find( ({id: projectId}) => projectId == id ).title = title
 
   return res.json(projects)
 })
 
-server.delete('/projects/:id', (req, res) => {
+server.post('/projects/:id/tasks', countRequest, checkIfProjectExists, (req, res) => {
+
+  const { id } = req.params
+  const { title } = req.body
+
+  projects.find(({id: projectId}) => projectId == id).tasks.push(title)
+
+  return res.json(projects)
+})
+
+server.delete('/projects/:id', countRequest, checkIfProjectExists, (req, res) => {
   projects = projects.filter(({id}) => Number(id) !== Number(req.params.id))
 
   return res.json(projects)
