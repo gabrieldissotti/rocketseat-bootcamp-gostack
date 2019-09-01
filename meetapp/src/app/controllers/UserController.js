ed64eb46ed72fa5874bc04e1c601c7e969b6223d
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-08-31 23:33:12 -0300

    feat(meetapp): password hash
    added bcrypt to hash password of created users

--- meetapp/src/app/controllers/UserController.js
+++ meetapp/src/app/controllers/UserController.js
@@ -1,9 +1,30 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      name: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(401).json({ error: 'Validations fails' });
+
+    const { email } = req.body;
+
+    if (await User.findOne({ where: { email } }))
+      return res
+        .status(401)
+        .json({ error: 'Already exists an user with this email' });
+
     try {
-      const { id, name, email } = await User.create(req.body);
+      const { id, name } = await User.create(req.body);
 
       return res.json({ id, name, email });
     } catch (error) {
