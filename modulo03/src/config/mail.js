0c99ac76ab991151a854a2e10556f67fa36467fc
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-01 21:14:32 -0300

    feat(appointments): notification by email
    added method to email provider about cancelling

---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8b72687c90202f',
+    pass: 'aeed267377ebd7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
