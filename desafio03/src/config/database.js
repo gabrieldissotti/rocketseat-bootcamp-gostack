f9aaaf9afe2e0458c98f101f44d5510f61264e6e
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-03 19:19:08 -0300

    feat(desafio03): users
    added api to create users without password hash and configured sequelize.

---
+++ desafio03/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'desafio03',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
