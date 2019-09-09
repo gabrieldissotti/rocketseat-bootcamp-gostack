6fc40ecd7843b00263e1f3fd1510d3c3a4711dfe
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-09 17:16:52 -0300

    feat(desafio04): header
    added header

--- desafio04/src/App.js
+++ desafio04/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import "./assets/css/reset.css";
+import "./assets/css/fonts.css";
+
 import "./App.css";
 
+import Header from "./components/Header";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ desafio04/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1 class="title">facebook.</h1>
+    </div>
+  );
+}
+
+export default Header;
