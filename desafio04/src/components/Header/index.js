033a0a9105381ef29f4a574c19f3850a299741e8
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-09 17:40:43 -0300

    feat(desafio04): header
    added link to perfil in header

--- desafio04/src/components/Header/index.js
+++ desafio04/src/components/Header/index.js
@@ -2,10 +2,20 @@ import React from "react";
 
 import "./style.css";
 
+import perfil from "../../assets/img/perfil.jpg";
+
 function Header() {
   return (
     <div className="header">
-      <h1 class="title">facebook.</h1>
+      <h1 className="title">facebook.</h1>
+      <div className="perfil">
+        <span>Meu perfil</span>
+        <img
+          className="perfil-photo"
+          src={perfil}
+          alt="Foto de perfil de Gabriel Dissotti"
+        />
+      </div>
     </div>
   );
 }
