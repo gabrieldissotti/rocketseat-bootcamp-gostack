707a49b43856ccfdac33f27aa9288dbc85eaa182
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-09 17:57:43 -0300

    feat(desafio04): post list
    added component post list to add many posts

--- desafio04/src/App.js
+++ desafio04/src/App.js
@@ -5,11 +5,13 @@ import "./assets/css/fonts.css";
 import "./App.css";
 
 import Header from "./components/Header";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <>
       <Header />
+      <PostList />
     </>
   );
 }
---
+++ desafio04/src/components/PostList/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css";
+
+function PostList() {
+  return <div className="post-list"></div>;
+}
+
+export default PostList;
