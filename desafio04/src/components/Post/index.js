c78bb916889d0b00e362a22832565a19fca076fc
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-09 18:02:21 -0300

    feat(desafio04): post
    added post component

---
+++ desafio04/src/components/Post/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css";
+
+function Post() {
+  return <div className="post"></div>;
+}
+
+export default Post;
--- desafio04/src/components/PostList/index.js
+++ desafio04/src/components/PostList/index.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import "./style.css";
 
+import Post from "../Post";
+
 function PostList() {
-  return <div className="post-list"></div>;
+  return (
+    <div className="post-list">
+      <Post />
+    </div>
+  );
 }
 
 export default PostList;
