1048daa060424b8877ec88728eff6d323b2a729a
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-12 18:13:51 -0300

    feat(modulo06): routes
    added navigation

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
 
 import "./config/ReactotronConfig";
 
+import Routes from "./routes";
+
 const App = () => {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
+  return <Routes />;
 };
 
 export default App;
