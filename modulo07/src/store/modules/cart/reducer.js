5a62a7fa000b39f8362f69c5e2ee1cdb418840fc
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-09-27 16:20:53 -0300

    feat(module07): added removing product

--- modulo07/src/store/modules/cart/reducer.js
+++ modulo07/src/store/modules/cart/reducer.js
@@ -1,8 +1,6 @@
 import produce from 'immer';
 
 export default function cart(state = [], action) {
-  console.log(state);
-
   switch (action.type) {
     case 'ADD_TO_CART':
       return produce(state, draft => {
@@ -15,6 +13,14 @@ export default function cart(state = [], action) {
             ...action.product,
             amount: 1,
           });
+        }
+      });
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
         }
       });
     default:
