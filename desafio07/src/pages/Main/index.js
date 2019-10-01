a3eddd0fc0e42c0522f2e3516ad4cf19652bdb09
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-10-01 17:45:32 -0300

    feat(home): added styles to home

--- desafio07/src/pages/Main/index.js
+++ desafio07/src/pages/Main/index.js
@@ -1,14 +1,63 @@
 import React from 'react';
 
-import { Container, ProductList, Item } from './styles';
+import {
+  Container,
+  ProductList,
+  Item,
+  Cover,
+  Legend,
+  Price,
+  Quantity,
+  Button,
+  Label,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <ProductList>
-        <Item />
-        <Item />
-        <Item />
+        <Item>
+          <Cover
+            source={{
+              uri:
+                'https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x',
+            }}
+          />
+          <Legend>Tênis de Caminhada Leve Confortável</Legend>
+          <Price>R$179,90</Price>
+          <Button>
+            <Quantity>1</Quantity>
+            <Label>ADICIONAR</Label>
+          </Button>
+        </Item>
+        <Item>
+          <Cover
+            source={{
+              uri:
+                'https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x',
+            }}
+          />
+          <Legend>Tênis de Caminhada Leve Confortável</Legend>
+          <Price>R$179,90</Price>
+          <Button>
+            <Quantity>1</Quantity>
+            <Label>ADICIONAR</Label>
+          </Button>
+        </Item>
+        <Item>
+          <Cover
+            source={{
+              uri:
+                'https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/30/D12-9119-130/D12-9119-130_detalhe2.jpg?ims=326x',
+            }}
+          />
+          <Legend>Tênis de Caminhada Leve Confortável</Legend>
+          <Price>R$179,90</Price>
+          <Button>
+            <Quantity>1</Quantity>
+            <Label>ADICIONAR</Label>
+          </Button>
+        </Item>
       </ProductList>
     </Container>
   );
--- desafio07/src/pages/Main/styles.js
+++ desafio07/src/pages/Main/styles.js
@@ -13,11 +13,69 @@ export const ProductList = styled.ScrollView.attrs({
 `;
 
 export const Item = styled.View`
+  position: relative;
+  margin-right: 15px;
+
   width: 220px;
   height: 358px;
-
-  margin-right: 15px;
+  padding: 10px;
 
   background-color: #fff;
   border-radius: 5px;
+`;
+
+export const Cover = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const Legend = styled.Text`
+  font-family: Roboto;
+  font-size: 16px;
+  line-height: 21px;
+`;
+
+export const Price = styled.Text`
+  margin-top: 5px;
+
+  font-family: Roboto;
+  font-size: 21px;
+  font-weight: bold;
+`;
+
+export const Button = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  bottom: 0;
+  margin: 10px;
+  position: absolute;
+
+  width: 100%;
+
+  overflow: hidden;
+  font-size: 14px;
+  background: #7159c1;
+  align-items: center;
+  border-radius: 5px;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const Quantity = styled.Text`
+  width: 25%;
+  height: 100%;
+  padding: 13px;
+
+  color: #fff;
+  text-align: right;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
+export const Label = styled.Text`
+  width: 75%;
+  padding: 13px;
+
+  color: #fff;
+  text-align: center;
+  font-weight: bold;
 `;
