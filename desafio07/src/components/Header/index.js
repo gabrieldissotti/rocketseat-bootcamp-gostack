665c24b947a402715a5e7ce02f5df07b42190dd7
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-10-01 12:28:03 -0300

    feat(header): added styled with styled-components

--- desafio07/src/components/Header/index.js
+++ desafio07/src/components/Header/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+
+import { Cart, Logo } from './styles';
 
 import LogoImage from '../../assets/images/logo.png';
 
 export default function Header() {
   return (
     <>
-      <Image source={LogoImage} style={{ width: 155, height: 20 }} />
-      <Text
-        style={{
-          color: '#fff',
-          backgroundColor: '#7159c1',
-          padding: 5,
-          borderRadius: 15,
-        }}
-      >
-        3
-      </Text>
+      <Logo source={LogoImage} />
+      <Cart>3</Cart>
     </>
   );
 }
---
+++ desafio07/src/components/Header/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Cart = styled.Text`
+  right: 10px;
+  position: absolute;
+
+  width: 16px;
+  height: 16px;
+
+  color: #fff;
+  font-size: 12px;
+  text-align: center;
+  line-height: 16px;
+  border-radius: 15px;
+  background-color: #7159c1;
+`;
+
+export const Logo = styled.Image`
+  width: 155;
+  height: 20;
+`;
