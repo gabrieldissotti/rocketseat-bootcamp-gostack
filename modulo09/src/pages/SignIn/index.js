bd45c41bd6938bb6cff8d34fd146aa53dc297a22
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-10-02 15:06:48 -0300

    feat(modulo09): added unformt to handle forms in signin and signup pages

--- modulo09/src/pages/SignIn/index.js
+++ modulo09/src/pages/SignIn/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form action="">
-        <input type="email" placeholder="seu e-mail" />
-        <input type="password" placeholder="sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="sua senha secreta"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form action="">
-        <input type="text" placeholder="nome completo" />
-        <input type="email" placeholder="seu e-mail" />
-        <input type="password" placeholder="sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="nome completo" />
+        <Input name="email" type="email" placeholder="seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="sua senha secreta"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
