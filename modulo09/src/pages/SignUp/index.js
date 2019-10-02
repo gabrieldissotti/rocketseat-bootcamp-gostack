459334832e5df9b75a67e668ac6e459ef9d487f4
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-10-02 15:00:00 -0300

    feat(modulo09): stylised singIn and signUp pages

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-export default function Signup() {
-  return <div />;
+export default function SignUp() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form action="">
+        <input type="text" placeholder="nome completo" />
+        <input type="email" placeholder="seu e-mail" />
+        <input type="password" placeholder="sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
