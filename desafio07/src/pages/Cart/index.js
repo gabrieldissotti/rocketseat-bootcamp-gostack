23e5c37d00e1246d8989e9ea1fea54a3c9d3ffde
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2019-10-01 22:38:01 -0300

    feat(cart): added styles to cart page

--- desafio07/src/pages/Cart/index.js
+++ desafio07/src/pages/Cart/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {
+  Card,
   Item,
   Name,
   Wrap,
@@ -15,34 +16,57 @@ import {
   SubTotal,
   BtnFinish,
   Container,
+  Description,
 } from './styles';
 
 export default function Cart() {
   return (
     <Container>
-      <Item>
-        <Image
-          source={{
-            uri:
-              'https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x',
-          }}
-        />
-        <Details>
-          <Name>Tênis de Caminhada Leve Confortável</Name>
-          <Price>R$179,90</Price>
-        </Details>
-        <Wrap>
-          <Amount>3</Amount>
-          <SubTotal>R$539,70</SubTotal>
-        </Wrap>
-      </Item>
-      <Total>
-        <Title>TOTAL</Title>
-        <Value>R$ 1619,10</Value>
-      </Total>
-      <BtnFinish>
-        <Label>FINALIZAR PEDIDO</Label>
-      </BtnFinish>
+      <Card>
+        <Item>
+          <Details>
+            <Image
+              source={{
+                uri:
+                  'https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x',
+              }}
+            />
+            <Description>
+              <Name>Tênis de Caminhada Leve Confortável</Name>
+              <Price>R$179,90</Price>
+            </Description>
+          </Details>
+          <Wrap>
+            <Amount>3</Amount>
+            <SubTotal>R$539,70</SubTotal>
+          </Wrap>
+        </Item>
+        <Item>
+          <Details>
+            <Image
+              source={{
+                uri:
+                  'https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x',
+              }}
+            />
+            <Description>
+              <Name>Tênis de Caminhada Leve Confortável</Name>
+              <Price>R$179,90</Price>
+            </Description>
+          </Details>
+          <Wrap>
+            <Amount>3</Amount>
+            <SubTotal>R$539,70</SubTotal>
+          </Wrap>
+        </Item>
+        <Total>
+          <Title>TOTAL</Title>
+          <Value>R$ 1619,10</Value>
+        </Total>
+        <BtnFinish>
+          <Label>FINALIZAR PEDIDO</Label>
+        </BtnFinish>
+      </Card>
     </Container>
   );
 }
--- desafio07/src/pages/Cart/styles.js
+++ desafio07/src/pages/Cart/styles.js
@@ -1,16 +1,104 @@
 import styled from 'styled-components';
 
-export const Item = styled.View``;
-export const Container = styled.View``;
-export const Image = styled.Image``;
-export const Details = styled.View``;
+export const Container = styled.View`
+  padding: 20px;
+  flex: 1;
+
+  width: 100%;
+
+  background-color: #191920;
+`;
+
+export const Card = styled.View`
+  padding: 22px 15px 15px 15px;
+
+  border-radius: 5px;
+  background-color: #fff;
+`;
+
+export const Item = styled.View`
+  margin-bottom: 20px;
+`;
+
+export const Description = styled.View`
+  margin: 10px 45px auto 10px;
+
+  flex-direction: column;
+`;
+
+export const Details = styled.View`
+  flex-direction: row;
+`;
+
+export const Image = styled.Image`
+  width: 80px;
+  height: 80px;
+`;
+
 export const Name = styled.Text``;
-export const Price = styled.Text``;
-export const Wrap = styled.View``;
-export const Amount = styled.Text``;
-export const SubTotal = styled.Text``;
+
+export const Price = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Wrap = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  border-radius: 5px;
+  background-color: #eee;
+  padding: 7px;
+`;
+
+export const Amount = styled.Text`
+  margin-left: 30px;
+
+  width: 51px;
+
+  padding: 5px 12px;
+
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  background-color: #fff;
+`;
+
+export const SubTotal = styled.Text`
+  position: absolute;
+  right: 10px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
 export const Total = styled.View``;
-export const Title = styled.Text``;
-export const Value = styled.Text``;
-export const BtnFinish = styled.TouchableOpacity``;
-export const Label = styled.Text``;
+
+export const Title = styled.Text`
+  color: #999;
+  text-align: center;
+  font-weight: bold;
+  font-family: Roboto;
+`;
+
+export const Value = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  font-family: Roboto;
+`;
+
+export const BtnFinish = styled.TouchableOpacity`
+  width: 100%;
+
+  padding: 13px;
+
+  border-radius: 5px;
+  background-color: #7159c1;
+`;
+
+export const Label = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  text-align: center;
+  font-family: Roboto;
+  font-weight: bold;
+`;
